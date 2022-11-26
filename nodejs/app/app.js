6bbe8fc91b0202783cb665033e3a2c166a442e19
Rafael Palamim <rafael.palamim@gmail.com>
2022-11-26 00:54:51 -0300

    Finalizando desafio 2

--- nodejs/app/app.js
+++ nodejs/app/app.js
@@ -1,28 +1,62 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const mysql = require('mysql2');
 
-const conn = mysql.createConnection({
-    host: 'db',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'fc'
-});
+var conn = null;
 
+function dbConnect() {
+    conn = mysql.createConnection({
+        host: 'db',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'fc'
+    });
 
-conn.connect();
+    conn.connect();
+}
 
-app.get('/', function (request, result) {
-    conn.query("SHOW DATABASES;", (err, results) => {
+app.get('/', function (request, response) {
+    dbConnect();
+    conn.query("SELECT id,name FROM fc.person", (err, results) => {
 
-        var html = '';
-        results.map((row) => {
-            html += row.Database + ' | ';
-        })
+        if(err){
+            response.send("Inicializando banco de dados... Aguarde.");
+            return;
+        }
 
-        result.send(html);
+        var html = '<h1>Full Cycle Rocks!</h1>';
+        html += "<h3>Cadastro de nome</h3>";
+        html += "<form method='post' action='http://localhost:8080/save'>";
+        html += "<input type='text' name='name' placeholder='Informe um nome' />"
+        html += "<button type='submit'>Salvar</button>"
+        html += "</form>";
+        html += "<h3>Lista de nomes</h3>";
+        if (results.length <= 0) {
+            html += "<p>Nenhum nome cadastrado. Use o campo acima para cadastrar.</p>";
+        } else {
+            html += "<ul>";
+            results.map((row) => {
+                html += `<li>ID ${row.id} - ${row.name}</li>`;
+            })
+            html += "</ul>";
+        }
+        response.send(html);
     })
+})
+app.post('/save', function (request, response) {
+    dbConnect();
+    conn.query(`INSERT INTO fc.person (name) VALUES ('${request.body.name}')`, (err, results) => {
+        if(err){
+            response.send("Inicializando banco de dados... Aguarde.");
+            return;
+        }
+        response.redirect('http://localhost:8080');
+    });
 })
 app.listen(3000, "0.0.0.0", function () {
     console.log('hello world app is listening on port 3000')
